232fde7af4ded618d710cfd890eb188ceb748884
JuanPenalverSanchez <82878367+JuanPenalverSanchez@users.noreply.github.com>
2022-09-06 19:20:26 +0200

    4 files modified, 2 added, 2 deleted

---
+++ styles/NavStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const NavStyle = styled.div`
+  .switchMode {
+    display: flex;
+    align-items: center;
+  }
+
+  .iconMode{
+    margin-right: 10px;
+  }
+`;
